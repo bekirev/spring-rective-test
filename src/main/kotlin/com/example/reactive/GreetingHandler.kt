e498e01315a7a10bdef863f2e46c906bd0faafb2
Artem Bekirev <abekirev@webetech.ru>
2021-10-13 18:32:04 +0300

    remove commented code

--- src/main/kotlin/com/example/reactive/GreetingHandler.kt
+++ src/main/kotlin/com/example/reactive/GreetingHandler.kt
@@ -2,7 +2,6 @@ package com.example.reactive
 
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.mono
-import org.slf4j.LoggerFactory
 import org.springframework.core.io.buffer.DataBuffer
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters
@@ -15,42 +14,7 @@ import reactor.core.publisher.Mono
 class GreetingHandler(
     private val requestHandler: RequestHandler
 ) {
-    companion object {
-        private val LOG = LoggerFactory.getLogger(GreetingHandler::class.java)
-    }
-
     fun hello(request: ServerRequest): Mono<ServerResponse> {
-//        request
-//            .bodyToMono(String::class.java)
-//            .subscribe { body ->
-//                println(
-//                    "Handle request [request = '${request}', headers = '${request.headers()}', body = '$body'"
-//                )
-//                LOG.info(
-//                    "Handle request [request = '{}', headers = '{}', body = '{}'",
-//                    request.toString(),
-//                    request.headers(),
-//                    body
-//                )
-//            }
-//            .body { inputMessage, _ ->
-//                inputMessage.body
-//                    .reduce(StringBuilder()) { acc, value ->
-//                        val stringPart = value.asOutputStream().use {
-//                            it.toString()
-//                        }
-//                        acc.append(stringPart)
-//                    }
-//                    .map(StringBuilder::toString)
-//                    .subscribe { body ->
-//                        LOG.info(
-//                            "Handle request [request = '{}', headers = '{}', body = '{}'",
-//                            request.toString(),
-//                            request.headers(),
-//                            body
-//                        )
-//                    }
-//            }
         return ServerResponse.ok()
             .body(
                 BodyInserters.fromPublisher(
